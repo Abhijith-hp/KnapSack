d5a2a9dd7fcf569ab14637347e06d42adba7dd16
abhijith_g1 <abhijith_g@geojit.com>
2025-09-26 16:12:03 +0530

    Approached bottom-up approach using space optimization by single array

--- knapsack.cpp
+++ knapsack.cpp
@@ -5,20 +5,20 @@ class Solution {
     int knapsack(int W, vector<int> &val, vector<int> &wt) {
         
         int n = val.size();
-        
+         
          vector<int>prev(W+1,0);
-         vector<int>curr(W+1,0);
+       
          
          for(int i=wt[0];i<=W;i++) prev[i] = val[0];
          
          for(int index=1;index<n;index++){
-             for(int weight = 0;weight<=W;weight++){
+             for(int weight = W;weight>=0;weight--){
                  int nonSteal = 0+prev[weight];
                  int steal = INT_MIN;
                  if(wt[index]<=weight) steal = val[index] + prev[weight-wt[index]];
-                 curr[weight]=max(steal,nonSteal);
+                 prev[weight]=max(steal,nonSteal);
              }
-            prev=curr;
+         
          }
          
          
