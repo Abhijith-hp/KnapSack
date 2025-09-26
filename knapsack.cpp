f8807e9d3f9eb2ae80f0c2c6776ad92c737b8857
abhijith_g1 <abhijith_g@geojit.com>
2025-09-26 16:10:05 +0530

    Implemented 0/1 Knapsack problem using tabulation

--- knapsack.cpp
+++ knapsack.cpp
@@ -1,26 +1,24 @@
 class Solution {
   public:
   
-    int compute(int index,int W,vector<int>&val,vector<int>&wt,vector<vector<int>>&dp){
-        
-        if(index==0){
-            if(wt[index]<=W) return val[0];
-            return 0;
-        }
-        if(dp[index][W]!=-1) return dp[index][W];
-        
-        int nonSteal = 0 + compute(index-1,W,val,wt,dp);
-        int steal = INT_MIN;
-        if(wt[index]<=W) steal = val[index] +compute(index-1,W-wt[index],val,wt,dp);
-         dp[index][W]= max(steal,nonSteal);
-         return dp[index][W];
-    }
+  
     int knapsack(int W, vector<int> &val, vector<int> &wt) {
         
-        int index = val.size();
-         vector<vector<int>>dp(index,vector<int>(W+1,-1));
+        int n = val.size();
+         vector<vector<int>>dp(n,vector<int>(W+1,0));
          
-         return compute(val.size()-1,W,val,wt,dp);
+         for(int i=wt[0];i<=W;i++) dp[0][i] = val[0];
+         
+         for(int index=1;index<n;index++){
+             for(int weight = 0;weight<=W;weight++){
+                 int nonSteal = 0+dp[index-1][weight];
+                 int steal = INT_MIN;
+                 if(wt[index]<=weight) steal = val[index] + dp[index-1][weight-wt[index]];
+                 dp[index][weight]=max(steal,nonSteal);
+             }
+         }
+         
+         return dp[n-1][W];
         
     }
 };
