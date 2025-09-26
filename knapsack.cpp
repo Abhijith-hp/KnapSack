48f83d0bb31741859e143023c821414d1b257aa3
abhijith_g1 <abhijith_g@geojit.com>
2025-09-26 16:09:18 +0530

    Implemented 0/1 Knapsack problem using memoziation

--- knapsack.cpp
+++ knapsack.cpp
@@ -1,20 +1,26 @@
 class Solution {
   public:
   
-    int compute(int index,int W,vector<int>&val,vector<int>&wt){
+    int compute(int index,int W,vector<int>&val,vector<int>&wt,vector<vector<int>>&dp){
         
         if(index==0){
             if(wt[index]<=W) return val[0];
             return 0;
         }
+        if(dp[index][W]!=-1) return dp[index][W];
         
-        int nonSteal = 0 + compute(index-1,W,val,wt);
+        int nonSteal = 0 + compute(index-1,W,val,wt,dp);
         int steal = INT_MIN;
-        if(wt[index]<=W) steal = val[index] +compute(index-1,W-wt[index],val,wt);
-         return max(steal,nonSteal);
+        if(wt[index]<=W) steal = val[index] +compute(index-1,W-wt[index],val,wt,dp);
+         dp[index][W]= max(steal,nonSteal);
+         return dp[index][W];
     }
     int knapsack(int W, vector<int> &val, vector<int> &wt) {
-         return compute(val.size()-1,W,val,wt);
+        
+        int index = val.size();
+         vector<vector<int>>dp(index,vector<int>(W+1,-1));
+         
+         return compute(val.size()-1,W,val,wt,dp);
         
     }
 };
