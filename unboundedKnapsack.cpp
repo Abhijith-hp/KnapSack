1683f9dc653fd844f00f07b77a7b9db25df692b7
abhijith_g1 <abhijith_g@geojit.com>
2025-09-28 18:47:23 +0530

    Implemented unbounded knapsack using tabulation

--- unboundedKnapsack.cpp
+++ unboundedKnapsack.cpp
@@ -2,21 +2,21 @@
 
 class Solution {
   public:
-    int selections(int index,int capacity,vector<int>&wt,vector<int>&val,vector<vector<int>>&dp){
-        if(index==0){
-            return ((capacity/wt[0]))*val[0];
-        }
-        if(dp[index][capacity]==-1) return dp[index][capacity];
-        
-        int nonTake = 0+selections(index-1,capacity,wt,val,dp);
-        int take = INT_MIN;
-        if(wt[index]<=capacity) take = val[index]+ selections(index,capacity-wt[index],wt,val,dp);
-        dp[index][capacity]=max(take,nonTake);
-        return dp[index][capacity];
-    }
+  
     int knapSack(vector<int>& val, vector<int>& wt, int capacity) {
         vector<vector<int>> dp(val.size(), vector<int>(capacity + 1, 0));
-        return selections(val.size()-1,capacity,wt,val,dp);
+        for(int i=0;i<=capacity;i++)
+            dp[0][i] = (i/wt[0]) * val[0];
+        
+        for(int i=1;i<val.size();i++){
+            for(int j=0;j<=capacity;j++){
+               int nonTake = 0+dp[i-1][j];
+        int take = INT_MIN;
+        if(wt[i]<=j) take = val[i]+ dp[i][j-wt[i]];
+        dp[i][j]=max(take,nonTake); 
+            }
+        }
+        return dp[val.size()-1][capacity];
         
     }
 };
