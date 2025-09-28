e57ac07ad145cf9f803bb714cbcaa034a5ee6ec7
abhijith_g1 <abhijith_g@geojit.com>
2025-09-28 18:47:59 +0530

    Implemented unbounded knapsack using tabulation by single array optimization

--- unboundedKnapsack.cpp
+++ unboundedKnapsack.cpp
@@ -1,22 +1,23 @@
-// User function Template for C++
-
-class Solution {
-  public:
-  
-    int knapSack(vector<int>& val, vector<int>& wt, int capacity) {
-        vector<vector<int>> dp(val.size(), vector<int>(capacity + 1, 0));
-        for(int i=0;i<=capacity;i++)
-            dp[0][i] = (i/wt[0]) * val[0];
-        
-        for(int i=1;i<val.size();i++){
-            for(int j=0;j<=capacity;j++){
-               int nonTake = 0+dp[i-1][j];
-        int take = INT_MIN;
-        if(wt[i]<=j) take = val[i]+ dp[i][j-wt[i]];
-        dp[i][j]=max(take,nonTake); 
+    // User function Template for C++
+    
+    class Solution {
+      public:
+      
+        int knapSack(vector<int>& val, vector<int>& wt, int capacity) {
+            vector<vector<int>> dp(val.size(), vector<int>(capacity + 1, 0));
+            vector<int>current(capacity+1,0);
+            for(int i=0;i<=capacity;i++)
+                current[i] = (i/wt[0]) * val[0];
+            
+            for(int i=1;i<val.size();i++){
+                for(int j=0;j<=capacity;j++){
+                   int nonTake = 0+current[j];
+            int take = INT_MIN;
+            if(wt[i]<=j) take = val[i]+ current[j-wt[i]];
+            current[j]=max(take,nonTake); 
+                }
             }
+            return current[capacity];
+            
         }
-        return dp[val.size()-1][capacity];
-        
-    }
-};
+    };
